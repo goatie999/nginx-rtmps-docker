75fcbea43eac9cb20b261314ded8f205e08fbc8b
goatie999 <markley79@gmail.com>
2020-04-28 02:14:51 +0100

    Handling 'ampersand'
    added sed code to insert escape character where Stream Keys like those for Facebook include the '&' character.  Making each one '\&'

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,8 @@ set -e
 
 if [ -n "${FACEBOOK_KEY}" ]; then
 	echo "Facebook activated"
-	sed -i 's|#facebook|push '"$FACEBOOK_URL""$FACEBOOK_KEY"';|g' /etc/nginx/nginx.conf 
+	FACEBOOK_KEY_esc=$(echo "$FACEBOOK_KEY" | sed 's/[\*\.&]/\\&/g')
+	sed -i 's|#facebook|push '"$FACEBOOK_URL""$FACEBOOK_KEY_esc"';|g' /etc/nginx/nginx.conf 
 else 
     echo "No Facebook stream key set... disabling"
     sed -i 's|#facebook| |g' /etc/nginx/nginx.conf 
@@ -13,11 +14,14 @@ fi
 
 if [ -n "${RESTREAM_KEY}" ]; then
 	echo "Restream.io activated"
+	RESTREAM_KEY_esc=$(echo "$RESTREAM_KEY" | sed 's/[\*\.&]/\\&/g')
 	sed -i 's|#restream|push '"$RESTREAM_URL""$RESTREAM_KEY"';|g' /etc/nginx/nginx.conf 
 else 
     echo "No Restream.io stream key set... disabling"
     sed -i 's|#restream| |g' /etc/nginx/nginx.conf 
 fi
+
+sed "s/%NAME%/${HNAME//&/\\&}/g" file
 
 stunnel4
 

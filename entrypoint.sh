9fd2f53da4ade7a50e01d796ff9c6b4db3029893
goatie999 <markley79@gmail.com>
2020-05-01 12:09:25 +0100

    YT & Twitch added

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,10 +15,28 @@ fi
 if [ -n "${RESTREAM_KEY}" ]; then
 	echo "Restream.io activated"
 	RESTREAM_KEY_esc=$(echo "$RESTREAM_KEY" | sed 's/[\*\.&]/\\&/g')
-	sed -i 's|#restream|push '"$RESTREAM_URL""$RESTREAM_KEY"';|g' /etc/nginx/nginx.conf 
+	sed -i 's|#restream|push '"$RESTREAM_URL""$RESTREAM_KEY_esc"';|g' /etc/nginx/nginx.conf 
 else 
     echo "No Restream.io stream key set... disabling"
     sed -i 's|#restream| |g' /etc/nginx/nginx.conf 
+fi
+
+if [ -n "${YOUTUBE_KEY}" ]; then
+	echo "YouTube activated"
+	YOUTUBE_KEY_esc=$(echo "$YOUTUBE_KEY" | sed 's/[\*\.&]/\\&/g')
+	sed -i 's|#youtube|push '"$YOUTUBE_URL""$YOUTUBE_KEY_esc"';|g' /etc/nginx/nginx.conf 
+else 
+    echo "No YouTube stream key set... disabling"
+    sed -i 's|#youtube| |g' /etc/nginx/nginx.conf 
+fi
+
+if [ -n "${TWITCH_KEY}" ]; then
+	echo "Twitch.tv activated"
+	TWITCH_KEY_esc=$(echo "$TWITCH_KEY" | sed 's/[\*\.&]/\\&/g')
+	sed -i 's|#twitch|push '"$TWITCH_URL""$TWITCH_KEY_esc"';|g' /etc/nginx/nginx.conf 
+else 
+    echo "No Twitch.tv stream key set... disabling"
+    sed -i 's|#twitch| |g' /etc/nginx/nginx.conf 
 fi
 
 if [ -n "${MIXCLOUD_KEY}" ]; then
